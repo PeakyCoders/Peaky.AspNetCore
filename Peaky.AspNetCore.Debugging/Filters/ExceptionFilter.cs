5dbd1e46fc165d826eca83bbb2ad6e224a6986dc
SnakyBeaky <urimr26cat@gmail.com>
2019-09-30 11:10:49 +0200

    Repository + project renames and framework update

--- Peaky.AspNetCore.Debugging/Filters/ExceptionFilter.cs
+++ Peaky.AspNetCore.Debugging/Filters/ExceptionFilter.cs
@@ -1,13 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Peaky.AspNetCore.Debugging.Static;
 
-namespace Peaky.Extensions.Web.Debug
+namespace Peaky.AspNetCore.Debugging.Filters
 {
     internal class ExceptionFilter : ExceptionFilterAttribute
     {
         public override void OnException(ExceptionContext ctx)
         {
             // Stores the exception in the HttpContext item bag for later usage in DebugMiddleware
-            ctx.HttpContext.Items[Constants.HttpContextDebugItem] = ctx.Exception;
+            ctx.HttpContext.Items[PeakyDebugConstants.HttpContextDebugItem] = ctx.Exception;
         }
     }
 }
